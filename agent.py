f4aed12090d5c9570e7143a6e97572b17a630106
nader <nader.zare@dal.ca>
2020-07-11 05:07:46 -0300

    action hint added to the simple agent

--- agent.py
+++ agent.py
@@ -7,7 +7,7 @@ env.render()
 print(obs)
 done = False
 while not done:
-    action = int(input('enter:'))
+    action = int(input('enter (0 Down, 1 Left, 2 Up, 3 Right:'))
     obs, reward, done = env.step(action)
     env.render()
     print(reward)
