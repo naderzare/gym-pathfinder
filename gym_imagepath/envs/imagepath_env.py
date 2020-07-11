2f7dddc9cd2b2bfd7fd6141a9b08db910a78fb95
nader <nader.zare@dal.ca>
2020-07-11 05:01:52 -0300

    some change in env

--- gym_imagepath/envs/imagepath_env.py
+++ gym_imagepath/envs/imagepath_env.py
@@ -1,6 +1,7 @@
 import gym
 from gym import spaces
 import numpy as np
+import random
 
 
 class ImagePathEnv(gym.Env):
@@ -9,19 +10,24 @@ class ImagePathEnv(gym.Env):
 
     def __init__(self):
         super(ImagePathEnv, self).__init__()
-        self.reward_range = (0, 1)
+        self.sparse_reward = False
+        self.count_i = 10
+        self.count_j = 10
         self.action_space = spaces.Box(low=np.array([0]), high=np.array([3]), dtype=np.float16)
-        self.observation_space = spaces.Box(low=0, high=1, shape=(10, 10), dtype=np.float16)
+        self.observation_space = spaces.Box(low=0, high=1, shape=(self.count_i, self.count_j), dtype=np.float16)
         self.current_step = 0
         self.viewer = None
         self.state = None
         self.agent_position = None
         self.goal_position = None
+        self.walls = None
 
     def _next_observation(self):
         obs = np.zeros(self.observation_space.shape)
         obs[self.agent_position[0], self.agent_position[1]] = 1
-        obs[self.goal_position[0], self.goal_position[1]] = 1
+        obs[self.goal_position[0], self.goal_position[1]] = 0.5
+        for w in self.walls:
+            obs[w[0], w[1]] = 0.25
         return obs
 
     def _take_action(self, action):
@@ -35,13 +41,29 @@ class ImagePathEnv(gym.Env):
             self.agent_position[1] += 1
 
     def step(self, action):
+        previous_position = [self.agent_position[0], self.agent_position[1]]
         self._take_action(action)
         self.current_step += 1
         reward = 0
-        if self.goal_position == self.agent_position:
-            reward = 1
+        if self.sparse_reward:
+            if self.goal_position == self.agent_position:
+                reward = 1
+            else:
+                reward = -1
         else:
-            reward = -1
+            if self.goal_position == self.agent_position:
+                reward = 1
+            elif self.agent_position[0] < 0 \
+                    or self.agent_position[0] > self.observation_space.shape[0] \
+                    or self.agent_position[1] < 0 or self.agent_position[1] > self.observation_space.shape[1] \
+                    or self.agent_position in self.walls:
+                reward = -1
+            else:
+                c_diff = abs(self.agent_position[0] - self.goal_position[0]) + abs(
+                    self.agent_position[1] - self.goal_position[1])
+                p_diff = abs(previous_position[0] - self.goal_position[0]) + abs(
+                    previous_position[1] - self.goal_position[1])
+                reward = (p_diff - c_diff) / 20
 
         done = bool(self.goal_position == self.agent_position
                     or self.agent_position[0] < 0
@@ -49,6 +71,7 @@ class ImagePathEnv(gym.Env):
                     or self.agent_position[1] < 0
                     or self.agent_position[1] > self.observation_space.shape[1]
                     or self.current_step > 20
+                    or self.agent_position in self.walls
                     )
 
         obs = None
@@ -58,8 +81,21 @@ class ImagePathEnv(gym.Env):
         return obs, reward, done
 
     def reset(self):
-        self.agent_position = [0, 0]
-        self.goal_position = [4, 4]
+        wall_inserted = 0
+        self.walls = []
+        while wall_inserted < 5:
+            wall = [random.randint(0, self.count_i - 1), random.randint(0, self.count_j - 1)]
+            if wall not in self.walls:
+                self.walls.append(wall)
+                wall_inserted += 1
+        while True:
+            self.agent_position = [random.randint(0, self.count_i - 1), random.randint(0, self.count_j - 1)]
+            if self.agent_position not in self.walls:
+                break
+        while True:
+            self.goal_position = [random.randint(0, self.count_i - 1), random.randint(0, self.count_j - 1)]
+            if self.goal_position != self.agent_position and self.goal_position not in self.walls:
+                break
         self.current_step = 0
         self.state = np.zeros(self.observation_space.shape)
         obs = self._next_observation()
@@ -73,11 +109,15 @@ class ImagePathEnv(gym.Env):
         cell_height = screen_height / self.observation_space.shape[1]
         if self.viewer is None:
             from gym.envs.classic_control import rendering
-            self.cells = [[None for i in range(self.observation_space.shape[0])] for j in range(self.observation_space.shape[1])]
+            self.cells = [[None for i in range(self.observation_space.shape[0])] for j in
+                          range(self.observation_space.shape[1])]
             self.viewer = rendering.Viewer(screen_width, screen_height)
-            for i in range(self.observation_space.shape[0]):
-                for j in range(self.observation_space.shape[1]):
-                    l, r, b, t = i * cell_width, (i + 1) * cell_width - 1, j * cell_height, (j + 1) * cell_height - 1
+            for i in range(self.observation_space.shape[1]):
+                for j in range(self.observation_space.shape[0]):
+                    l, r, b, t = j * cell_width, \
+                                 (j + 1) * cell_width - 1, \
+                                 (self.observation_space.shape[0] - i - 1) * cell_height, \
+                                 (self.observation_space.shape[0] - i) * cell_height - 1
                     cell = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
                     cell.set_color(0.5, 0.5, 0.5)
                     self.cells[i][j] = cell
@@ -90,6 +130,8 @@ class ImagePathEnv(gym.Env):
                     cell.set_color(0, 0, 1)
                 elif self.goal_position == [i, j]:
                     cell.set_color(0, 1, 0)
+                elif [i, j] in self.walls:
+                    cell.set_color(0., 0., 0.)
                 else:
                     cell.set_color(0.5, 0.5, 0.5)
                 self.viewer.add_geom(cell)
