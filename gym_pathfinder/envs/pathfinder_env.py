a17ea7963f4f29f54a22323c0018354b465de181
nader <nader.zare@dal.ca>
2020-07-23 19:32:34 -0300

    add rotation reset

--- gym_pathfinder/envs/pathfinder_env.py
+++ gym_pathfinder/envs/pathfinder_env.py
@@ -29,7 +29,10 @@ class PathFinderEnv(gym.Env):
         self.random_walls = 5
         self.map_path = map_path
         self.maps = []
+        self.rot_maps = []
         self.read_maps()
+        self.time_neg_reward = False
+        self.move_neg_reward = False
 
     def read_maps(self):
         if self.map_path is None:
@@ -42,7 +45,9 @@ class PathFinderEnv(gym.Env):
 
     def read_map(self, path):
         f = open(path, 'r').readline()
-        self.maps.append(eval(f))
+        vmap = eval(f)
+        self.maps.append(vmap)
+        self.rot_maps.append([w[1], w[0]] for w in vmap)
 
     def _next_observation(self, done):
         obs = np.zeros(self.observation_space.shape)
@@ -71,25 +76,25 @@ class PathFinderEnv(gym.Env):
         done = False
         if self.sparse_reward:
             if len(next_agent_position[0]) == 0:
-                reward = 1
+                reward = 2
                 done = True
             elif goal_position == next_agent_position:
-                reward = 1
+                reward = 2
                 done = True
             else:
-                reward = -1
+                reward = -2
         else:
             if len(next_agent_position[0]) == 0:
-                reward = 1
+                reward = 2
                 done = True
             elif goal_position == next_agent_position:
-                reward = 1
+                reward = 2
                 done = True
             elif next_agent_position[0] < 0 \
                     or next_agent_position[0] > self.observation_space.shape[0] \
                     or next_agent_position[1] < 0 or next_agent_position[1] > self.observation_space.shape[1] \
                     or next_agent_position in self.walls:
-                reward = -1
+                reward = -2
                 done = True
             else:
                 c_diff = abs(next_agent_position[0] - goal_position[0]) + abs(
@@ -97,8 +102,11 @@ class PathFinderEnv(gym.Env):
                 p_diff = abs(agent_position[0] - goal_position[0]) + abs(
                     agent_position[1] - goal_position[1])
                 reward = (p_diff - c_diff) / 20
-                if len(reward) == 0:
-                    print(state.reshape((10, 10)))
+                if self.move_neg_reward:
+                    reward -= 0.02
+                if self.time_neg_reward:
+                    if self.current_step > 50:
+                        reward = -2
         return reward, done
 
     def step(self, action):
@@ -108,23 +116,29 @@ class PathFinderEnv(gym.Env):
         reward = 0
         if self.sparse_reward:
             if self.goal_position == self.agent_position:
-                reward = 1
+                reward = 2
             else:
-                reward = -1
+                reward = -2
         else:
             if self.goal_position == self.agent_position:
-                reward = 1
+                reward = 2
             elif self.agent_position[0] < 0 \
                     or self.agent_position[0] > self.observation_space.shape[0] \
                     or self.agent_position[1] < 0 or self.agent_position[1] > self.observation_space.shape[1] \
                     or self.agent_position in self.walls:
-                reward = -1
+                reward = -2
             else:
                 c_diff = abs(self.agent_position[0] - self.goal_position[0]) + abs(
                     self.agent_position[1] - self.goal_position[1])
                 p_diff = abs(previous_position[0] - self.goal_position[0]) + abs(
                     previous_position[1] - self.goal_position[1])
-                reward = (p_diff - c_diff) / 20
+                reward = (p_diff - c_diff) / 50
+
+                if self.move_neg_reward:
+                    reward -= 0.02
+                if self.time_neg_reward:
+                    if self.current_step > 50:
+                        reward = -2
 
         done = bool(self.goal_position == self.agent_position
                     or self.agent_position[0] < 0
@@ -132,7 +146,7 @@ class PathFinderEnv(gym.Env):
                     or self.agent_position[1] < 0
                     or self.agent_position[1] > self.observation_space.shape[1] - 1
                     or self.agent_position in self.walls
-                    or self.current_step > 30
+                    or self.current_step > 50
                     )
         information = {'result': 'normal'}
         if done:
@@ -140,7 +154,7 @@ class PathFinderEnv(gym.Env):
                 information['result'] = 'goal'
             elif self.agent_position in self.walls:
                 information['result'] = 'wall'
-            elif self.current_step > 30:
+            elif self.current_step > 50:
                 information['result'] = 'time'
             else:
                 information['result'] = 'out'
@@ -172,10 +186,16 @@ class PathFinderEnv(gym.Env):
         return False
 
     def reset(self):
+        self.reset_with_rot(False)
+
+    def reset_with_rot(self, test_rot):
         wall_inserted = 0
         free_cell = [[i, j] for i in range(10) for j in range(10)]
         self.walls = []
-        self.current_step = random.choice(self.maps)
+        if test_rot:
+            self.current_step = random.choice(self.rot_maps)
+        else:
+            self.current_step = random.choice(self.maps)
         for w in self.current_step:
             self.walls.append(w)
             free_cell.remove(w)
