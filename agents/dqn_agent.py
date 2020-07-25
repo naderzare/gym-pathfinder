bb1f31972f1dff1164b9117312e1c6607b4e4164
nader <nader.zare@dal.ca>
2020-07-24 21:30:17 -0300

    update args

--- agents/dqn_agent.py
+++ agents/dqn_agent.py
@@ -30,7 +30,7 @@ parser.add_argument('--mnr', help='move negative reward', type=str2bool, default
 parser.add_argument('-r', '--rl_rotating', help='using rotating in training', type=str2bool, default=False)
 parser.add_argument('-t', '--test', help='just test', type=str2bool, default=False)
 parser.add_argument('-tr', '--test_rotating', help='test_rotating', type=str2bool, default=True)
-parser.add_argument('-uh', '--use_her', help='using HER', type=str2bool)
+parser.add_argument('-uh', '--use_her', help='using HER', type=str2bool, default=False)
 parser.add_argument('-ht', '--her_type', help='HER Type', type=str, default='future')
 parser.add_argument('-hn', '--her_number', help='HER Number', type=int, default=4)
 parser.add_argument('-n', '--name', help='Run Name', type=str, default='test_'+str(time.time()))
