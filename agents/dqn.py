3514751531c06f37e9a871ea0285f52c4b4363fd
Nader Zare <nader.zare88@gmail.com>
2020-12-09 11:33:38 -0400

    add double and more

--- agents/dqn.py
+++ agents/dqn.py
@@ -16,7 +16,7 @@ from agents.transit import Transition
 class DeepQ:
     def __init__(self, buffer_size=100000, train_interval_step=100, target_update_interval_step=200, prb=False, train_step_counter=3200):
         self.model_type = ''
-        self.model = None
+        self.online_network = None
         self.target_network = None
         self.prb = prb
         self.buffer = Buffer(buffer_size)
@@ -34,6 +34,7 @@ class DeepQ:
         self.rotating = False
         self.max_rotating = False
         self.max_rotating_function = 'max'
+        self.rl_type = 'dqn'
         pass
 
     def create_model_cnn_dense(self):
@@ -51,8 +52,8 @@ class DeepQ:
         model = keras.Model(in1, out)
         model.compile(optimizer=optimizers.Adam(lr=0.00025), loss=losses.mse, metrics=[metrics.mse])
         model.summary()
-        self.model = model
-        self.target_network = keras.models.clone_model(self.model)
+        self.online_network = model
+        self.target_network = keras.models.clone_model(self.online_network)
 
     def create_model_dense(self, input_shape=(4,)):
         self.model_type = 'param'
@@ -63,26 +64,26 @@ class DeepQ:
         model.add(layers.Dense(9, activation=activations.linear))
         model.compile(optimizer=optimizers.RMSprop(lr=0.00025, rho=0.95), loss=losses.mse, metrics=[metrics.mse])
         model.summary()
-        self.model = model
-        self.target_network = keras.models.clone_model(self.model)
+        self.online_network = model
+        self.target_network = keras.models.clone_model(self.online_network)
 
     def read_model(self, path, model_type):
         self.model_type = model_type
-        self.model = keras.models.load_model(path)
-        self.model.compile(optimizer=optimizers.RMSprop(lr=0.00025, rho=0.95), loss=losses.mse, metrics=[metrics.mse])
-        self.target_network = keras.models.clone_model(self.model)
-        print(self.model.summary())
+        self.online_network = keras.models.load_model(path)
+        self.online_network.compile(optimizer=optimizers.RMSprop(lr=0.00025, rho=0.95), loss=losses.mse, metrics=[metrics.mse])
+        self.target_network = keras.models.clone_model(self.online_network)
+        print(self.online_network.summary())
 
     def read_weight(self, path):
-        self.model.load_weights(path)
-        self.target_network = keras.models.clone_model(self.model)
+        self.online_network.load_weights(path)
+        self.target_network = keras.models.clone_model(self.online_network)
 
     def get_q(self, transits: List[Transition]):
         x = []
         for t in transits:
             x.append(t.state)
         x = array(x)
-        y = self.model.predict(x)
+        y = self.online_network.predict(x)
         return y
 
     def get_best_action(self, state):
@@ -93,7 +94,7 @@ class DeepQ:
             if state.ndim == 1:
                 state = state.reshape((1, state.shape[0]))
 
-        Y = self.model.predict(state)
+        Y = self.online_network.predict(state)
         actions = np.argmax(Y, axis=1)
         max_qs = np.max(Y, axis=1).flatten()
 
@@ -149,16 +150,12 @@ class DeepQ:
         if self.step_number % self.train_interval_step == 0:
             self.update_from_buffer()
         if self.step_number % self.target_update_interval_step == 0:
-            self.target_network.set_weights(self.model.get_weights())
+            self.target_network.set_weights(self.online_network.get_weights())
 
     def update_from_buffer(self):
         transits: List[Transition] = self.buffer.get_rand(self.train_step_counter)
         if len(transits) == 0:
             return
-        is_image_param = False
-        if self.model_type == 'imageparam':
-            is_image_param = True
-
         states_view = []
         next_states_view1 = []
         next_states_view2 = []
@@ -181,7 +178,6 @@ class DeepQ:
                     next_states_view3.append(t.next_state[2])
                     next_states_view4.append(t.next_state[3])
 
-
         states_view = array(states_view)
         next_states_view1 = array(next_states_view1)
         if self.max_rotating:
@@ -189,18 +185,47 @@ class DeepQ:
             next_states_view3 = array(next_states_view3)
             next_states_view4 = array(next_states_view4)
 
-        q = self.model.predict(states_view)
+        q = self.online_network.predict(states_view)
         best_q_action = np.argmax(q, axis=1)
 
         next_q1 = self.target_network.predict(next_states_view1)
-        next_states_max_q1 = np.max(next_q1, axis=1).flatten()
+        if self.rl_type == 'double':
+            next_states_max_q = []
+            for i in best_q_action:
+                next_states_max_q.append(next_q1[len(next_states_max_q)][i])
+            next_states_max_q1 = np.array(next_states_max_q)
+        else:
+            next_states_max_q1 = np.max(next_q1, axis=1).flatten()
+
         if self.max_rotating:
             next_q2 = self.target_network.predict(next_states_view2)
             next_q3 = self.target_network.predict(next_states_view3)
             next_q4 = self.target_network.predict(next_states_view4)
-            next_states_max_q2 = np.max(next_q2, axis=1).flatten()
-            next_states_max_q3 = np.max(next_q3, axis=1).flatten()
-            next_states_max_q4 = np.max(next_q4, axis=1).flatten()
+            if self.rl_type == 'double':
+                next_states_max_q = []
+                for i in best_q_action:
+                    next_states_max_q.append(next_q2[len(next_states_max_q)][i])
+                next_states_max_q2 = np.array(next_states_max_q)
+            else:
+                next_states_max_q2 = np.max(next_q2, axis=1).flatten()
+            if self.rl_type == 'double':
+                next_states_max_q = []
+                for i in best_q_action:
+                    next_states_max_q.append(next_q3[len(next_states_max_q)][i])
+                next_states_max_q3 = np.array(next_states_max_q)
+            else:
+                next_states_max_q3 = np.max(next_q3, axis=1).flatten()
+            if self.rl_type == 'double':
+                next_states_max_q = []
+                for i in best_q_action:
+                    next_states_max_q.append(next_q4[len(next_states_max_q)][i])
+                next_states_max_q4 = np.array(next_states_max_q)
+            else:
+                next_states_max_q4 = np.max(next_q4, axis=1).flatten()
+
+            # next_states_max_q2 = np.max(next_q2, axis=1).flatten()
+            # next_states_max_q3 = np.max(next_q3, axis=1).flatten()
+            # next_states_max_q4 = np.max(next_q4, axis=1).flatten()
 
         for i in range(len(transits)):
             q_learning = transits[i].reward
@@ -212,22 +237,21 @@ class DeepQ:
                         next_q_max = min(next_states_max_q1[i], next_states_max_q2[i], next_states_max_q3[i], next_states_max_q4[i])
                     elif self.max_rotating_function == 'avg':
                         next_q_max = (next_states_max_q1[i] + next_states_max_q2[i] + next_states_max_q3[i] + next_states_max_q4[i]) / 4.0
-                    elif self.max_rotating_function == 'minmax':
-                        if self.episode_number < 33000:
-                            next_q_max = min(next_states_max_q1[i], next_states_max_q2[i], next_states_max_q3[i],next_states_max_q4[i])
-                        elif self.episode_number < 66000:
-                            next_q_max = (next_states_max_q1[i] + next_states_max_q2[i] + next_states_max_q3[i] + next_states_max_q4[i]) / 4.0
-                        else:
-                            next_q_max = max(next_states_max_q1[i], next_states_max_q2[i], next_states_max_q3[i],next_states_max_q4[i])
+                    elif self.max_rotating_function == 'random':
+                        all_next_q = [next_states_max_q1[i], next_states_max_q2[i], next_states_max_q3[i],
+                                         next_states_max_q4[i]]
+                        next_q_max = all_next_q[random.randint(0, 3)]
                     else:
                         next_q_max = next_states_max_q1[i]
                 else:
                     next_q_max = next_states_max_q1[i]
                 q_learning += (self.gama * next_q_max)
+            if self.rl_type == 'more':
+                q_learning = min(q_learning, 5)
             diff = (q_learning - q[i][transits[i].action]) * transits[i].value
             q[i][transits[i].action] += diff
 
-        history = self.model.fit(states_view, q,  epochs=1, batch_size=32, verbose=0)
+        history = self.online_network.fit(states_view, q, epochs=1, batch_size=32, verbose=0)
         history_dict = history.history
         loss_values = history_dict['loss']
         self.loss_values.append(loss_values[0])
